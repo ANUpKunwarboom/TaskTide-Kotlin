48a24042bd09b2846d57396ca5e68243c7ea3de6
ANUpKunwarboom <anupkunwar340@gmail.com>
2026-07-17 11:22:33 +0545

    Added SplashActivity file

--- app/src/main/java/com/example/todolistapp/SplashActivity.kt
+++ app/src/main/java/com/example/todolistapp/SplashActivity.kt
@@ -7,6 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import com.example.todolistapp.navigation.NavGraph
 import com.example.todolistapp.ui.theme.TodoListAppTheme
 
+/**
+ * SplashActivity serves as the entry point of the TaskTide application.
+ * It initializes the theme and sets up the navigation graph.
+ */
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
